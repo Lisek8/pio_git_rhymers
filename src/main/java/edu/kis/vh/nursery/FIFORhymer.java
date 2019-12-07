41b1d528bb943e668d2d62222b396ad661688818
Lisek8 <fafik989@gmail.com>
2019-12-07 23:06:55 +0100

    1.5 Testy, diagramy, dokumentacja

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,13 +1,23 @@
 package edu.kis.vh.nursery;
 
+/**
+ * FIFO Rhymer class
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * DefaultCountingOutRhymer used as a base for FIFORhymer
+     */
     private DefaultCountingOutRhymer temporaryRhymer = new DefaultCountingOutRhymer();
 
+    /**
+     * Reimplementation of countOut method from DefaultCountingOutRhymer
+     * @return last inputed number or -1 if stack is empty
+     */
     @Override
     public int countOut() {
         while (!callCheck())
-			
+
             temporaryRhymer.countIn(super.countOut());
 
         int ret = temporaryRhymer.countOut();
